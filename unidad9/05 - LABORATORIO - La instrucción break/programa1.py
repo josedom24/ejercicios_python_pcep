7ae688b7dc930c119e3370b62ff6046f179b1a01
José Domingo Muñoz <josedom24@gmail.com>
2024-09-30 16:31:12 +0200

    Unidad9

---
+++ unidad9/05 - LABORATORIO - La instrucción break/programa1.py
@@ -0,0 +1,6 @@
+secreto = "chupacabra"
+while True:
+    clave = input("Dime la clave:")
+    if clave==secreto:
+        break
+print("¡Has dejado el bucle con éxito!!!")
---
+++ unidad9/05 - LABORATORIO - La instrucción break/programa2.py
@@ -0,0 +1,6 @@
+secreto = "chupacabra"
+clave = input("Dime la clave:")
+while clave != secreto:
+    print("Clave incorrecta!!!")
+    clave = input("Dime la clave:")
+print("¡Has dejado el bucle con éxito!!!")
