23c231b1d69fdeb29201d500368ece45d4f6172e
José Domingo Muñoz <josedom24@gmail.com>
2024-09-30 17:56:39 +0200

    Unidad11

---
+++ unidad11/01 - LABORATORIO - Los fundamentos de las listas/programa.py
@@ -0,0 +1,16 @@
+hat_list = [1, 2, 3, 4, 5]  # Esta es una lista existente de números ocultos en el sombrero.
+
+# Paso 1: escribe una línea de código que solicite al usuario
+# reemplazar el número de en medio con un número entero ingresado por el usuario.
+
+hat_list[2] = int(input("Número:"))
+
+# Paso 2: escribe aquí una línea de código que elimine el último elemento de la lista.
+
+del hat_list[4]
+
+# Paso 3: escribe aquí una línea de código que imprima la longitud de la lista existente.
+
+print(len(hat_list))
+
+print(hat_list)
