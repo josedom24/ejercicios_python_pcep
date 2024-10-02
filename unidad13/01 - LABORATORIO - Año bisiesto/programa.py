7658a3f7771efd045853bf617b4dbb1ed576256e
José Domingo Muñoz <josedom24@gmail.com>
2024-10-02 21:58:20 +0200

    Unidad13

---
+++ unidad13/01 - LABORATORIO - Año bisiesto/programa.py
@@ -0,0 +1,26 @@
+def año_bisiesto(año):
+    if año % 4 != 0:
+        return False
+    elif año % 100 != 0:
+        return True
+    elif año % 400 != 0:
+        return False
+    else:
+        return True
+
+test_data = [1900, 2000, 2016, 1987]
+test_results = [False, True, True, False]
+for i in range(len(test_data)):
+    yr = test_data[i]
+    print(yr,"->",end="")
+    result = año_bisiesto(yr)
+    if result == test_results[i]:
+        print("OK")
+    else:
+        print("Fallido")
+
+
+    
+
+
+
