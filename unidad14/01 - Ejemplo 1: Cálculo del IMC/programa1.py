6973372710566550982ac853d58933d3b067647e
José Domingo Muñoz <josedom24@gmail.com>
2024-10-03 17:11:24 +0200

    Unidad14

---
+++ unidad14/01 - Ejemplo 1: Cálculo del IMC/programa1.py
@@ -0,0 +1,4 @@
+def imc(weight, height):
+    return weight / height ** 2
+
+print(imc(52.5, 1.65))
---
+++ unidad14/01 - Ejemplo 1: Cálculo del IMC/programa2.py
@@ -0,0 +1,9 @@
+def imc(weight, height):
+    if height < 1.0 or height > 2.5 or \
+    weight < 20 or weight > 200:
+        return None
+
+    return weight / height ** 2
+
+
+print(imc(352.5, 1.65))
