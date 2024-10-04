cda0e89e53b775210de54a9a6f85fb5a815740f2
José Domingo Muñoz <josedom24@gmail.com>
2024-10-04 21:16:00 +0200

    Unidad16

---
+++ unidad16/01 - Pruebas de ejecución/programa2.py
@@ -0,0 +1,7 @@
+lista = [3, 5, 7, 8]
+suma=0
+for num in lista:
+    print(num) # Quiero saber que valor va tomando la variable de control del bucle
+    suma += num
+    print(suma) # Quiero saber que valor se va acumulando en la variable suma
+print(suma)
