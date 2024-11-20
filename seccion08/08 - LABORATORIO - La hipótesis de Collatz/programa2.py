77c37c1261bb51d4eea17f52e334d856da5f43d5
José Domingo Muñoz Rodríguez <josedom24@gmail.com>
2024-11-20 20:48:15 +0100

    Update programa2.py

--- seccion08/08 - LABORATORIO - La hipótesis de Collatz/programa2.py
+++ seccion08/08 - LABORATORIO - La hipótesis de Collatz/programa2.py
@@ -1,5 +1,6 @@
 
 for c0 in range(1,11):
+    pasos=0
     print("Número:", c0)
     print (20 * "-")
     while c0 != 1:
@@ -7,5 +8,7 @@ for c0 in range(1,11):
             c0 = c0 // 2
         else:
             c0 = 3 * c0 +1
+        pasos=pasos+1
         print(c0)
+    print("Pasos:",pasos)
     
