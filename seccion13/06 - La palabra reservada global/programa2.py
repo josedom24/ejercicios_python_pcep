204ae4d783ba1605a91991484559da415b509e53
José Domingo Muñoz Rodríguez <josedom24@gmail.com>
2024-11-22 12:39:56 +0100

    Update programa2.py

--- seccion13/06 - La palabra reservada global/programa2.py
+++ seccion13/06 - La palabra reservada global/programa2.py
@@ -1,11 +1,9 @@
-def my_function(my_list_1):
-    print("Print #1:", my_list_1)
-    print("Print #2:", my_list_2)
-    my_list_1 = [0, 1]
-    print("Print #3:", my_list_1)
-    print("Print #4:", my_list_2)
+def my_function(n):
+    print("Yo recibí", n)
+    n += 1
+    print("Ahora tengo", n)
 
 
-my_list_2 = [2, 3]
-my_function(my_list_2)
-print("Print #5:", my_list_2)
+var = 1
+my_function(var)
+print(var)
